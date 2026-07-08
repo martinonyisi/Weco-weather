9ac92a6c605d1ec01ddc424eed91a161bfd66538
Martony <martinonyisi1@gmail.com>
2026-07-08 15:18:00 +0100

    Create sw.js
    For offline

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Service Worker for WeCo Weather App
+const CACHE_NAME = 'weco-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
